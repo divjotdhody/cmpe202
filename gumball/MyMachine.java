1ced28dad369a026f43bf55aac1ed07247bf8285
unknown <divjot.dhody@sjsu.edu>
2019-09-22 21:43:45 -0700

    Result Snapshot

--- gumball/MyMachine.java
+++ gumball/MyMachine.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+
 public class GumballMachine
 {
 
     private int num_gumballs;
     private boolean has_quarter;
+    public ArrayList<Integer> coins_passed;
+    public int remaining_amount;
+    public int amount_passed;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size , int amount_passed , ArrayList<Integer> coins_passed )
     {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
+        this.amount_passed = amount_passed;
+        this.coins_passed = coins_passed;
+        this.remaining_amount = 0;
     }
 
     public void insertQuarter(int coin)
